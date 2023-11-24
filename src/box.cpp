f101324f284c9de9a8a671138544552456eb28fb
Alejandro Ortega <ort18248@uvg.edu.gt>
2023-11-24 09:01:18 -0600

    Update: Box rayIntersect now calculates uv coords for the hit point

--- src/box.cpp
+++ src/box.cpp
@@ -48,18 +48,28 @@ Intersect Box::rayIntersect(const Ray &ray) const {
 	// Normal calculation method from:
 	// https://www.reddit.com/r/raytracing/comments/yxaabc/ray_box_intersection_normal/
 	// https://www.shadertoy.com/view/wtSyRd
+	
 	// Intersection point on box
 	glm::vec3 hitPoint = ray.origin + (ray.direction * distance);
 	// Center of the box
 	glm::vec3 boxCenter = (bounds[1] + bounds[0]) / 2.0f;
 	glm::vec3 boxVec = hitPoint - boxCenter; // Vector from hitPoint to center
 
+	// Calculate UV coordinates based on the intersection point
+    float u = (hitPoint.x - bounds[0].x) / (bounds[1].x - bounds[0].x);
+    float v = (hitPoint.y - bounds[0].y) / (bounds[1].y - bounds[0].y);
+
+    // Clamp UV coordinates to [0, 1] range
+    u = glm::clamp(u, 0.0f, 1.0f);
+    v = glm::clamp(v, 0.0f, 1.0f);
+
+	// Normal Calculation
 	glm::vec3 normal;
 	normal = boxVec / std::max(std::max(std::abs(boxVec.x), std::abs(boxVec.y)), std::abs(boxVec.z));
 	normal = glm::clamp(normal, glm::vec3(0, 0, 0), glm::vec3(1, 1, 1));
 	normal = glm::normalize(glm::floor(normal * 1.0000001f)); // Unit normal for hitPoint
 
-    return Intersect{true, distance, hitPoint, normal};
+    return Intersect{true, distance, hitPoint, normal, glm::vec2(u, v)};
 }
 
 Intersect Box::rayIntersect(const glm::vec3& rayOrigin, const glm::vec3& rayDirection) const {
